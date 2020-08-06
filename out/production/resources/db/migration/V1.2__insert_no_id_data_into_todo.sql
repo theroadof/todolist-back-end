97141b3d47f7cbe956155bd49272beca6e90cbc6
theroadof <1294911641@qq.com>
2020-08-06 22:13:42 +0800

    transfer repository

---
+++ out/production/resources/db/migration/V1.2__insert_no_id_data_into_todo.sql
@@ -0,0 +1,4 @@
+insert into todo (text,status) values ('oocl',true);
+insert into todo (text,status) values ('good good study',false);
+insert into todo (text,status) values ('day day up',false);
+insert into todo (text,status) values ('zero',true);
